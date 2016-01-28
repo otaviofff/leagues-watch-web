8f7b7e9da9c2940da02da2a224d93687447e3713
Otavio Ferreira <otavio.ferreira@gmail.com>
2016-01-28 19:14:23 -0200

    Class App updated in order to cater for dynamic port assignment (Heroku)

--- src/main/java/watch/leagues/web/App.java
+++ src/main/java/watch/leagues/web/App.java
@@ -1,6 +1,7 @@
 package watch.leagues.web;
 
 import static spark.Spark.get;
+import static spark.SparkBase.port;
 
 import java.util.Date;
 
@@ -11,14 +12,42 @@ public class App {
     /***
      * Application entry point.
      * 
-     * It will serve all static files are under "/public" in classpath, 
-     *   if the request URL isn't addressed by others routes. When using Maven, 
-     *   the "/public" folder is assumed to be in "/main/resources".
-     * 
      * @param String args
      */
     public static void main(String[] args) {
-        Spark.staticFileLocation("/public");
+        listenToAssignedPort();
+        renderPublicAssets();
+        
         get("/ping", (request, response) -> "OK " + new Date().toString());
+    }
+    
+    /**
+     * Makes the application listen to the port assigned to it by the hosting
+     *  service in question, such as Heroku. If no port number is found in
+     *  the environment, then the default Spark port (4567) will be set.
+     *  
+     */
+    private static void listenToAssignedPort() {
+        ProcessBuilder builder = new ProcessBuilder();
+        String value = builder.environment().get("PORT");
+        Integer port = 4567; //Default port set by Spark.
+        
+        if (value != null) {
+            port = Integer.valueOf(value);
+        }
+        
+        port(port);
+    }
+    
+    /**
+     * Sets the location of all public assets in the application, 
+     *  such as HTML, CSS and JS files. This function will serve all static 
+     *  files under "/public" in classpath, if the request URL isn't 
+     *  addressed by others routes. When using Maven, the "/public" folder is 
+     *  assumed to be in "/main/resources".
+     *  
+     */
+    private static void renderPublicAssets() {
+        Spark.staticFileLocation("/public");
     }
 }
